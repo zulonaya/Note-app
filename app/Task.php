7b732c467f5da22c9ebf578315b59fa6cf14e189
zulfathly <zulfathly@devwiz.my>
2018-09-09 23:58:52 +0800

    first commit of the project. Interface to be redone

---
+++ app/Task.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+	public $timestamps = false;
+
+	public function toggleStatus()
+	{
+		$this->status = ! $this ->status;
+		return $this;
+	}
+    //
+}
